acedbd8f12622526f1e619a8ab9af73d4a633d5e
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2026-04-01 15:16:54 +0000

    Diagnosing background removal.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -503,10 +503,12 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
 
     async uploadFile(fileName: string, file: File): Promise<string> {
         // Don't honor file's name; want to overwrite existing content that may have had a different actual name.
-        const updateResponse = await this.storage.set(fileName, file).forUser();
+        const updateResponse = await this.storage.set(fileName, file);
         if (!updateResponse.data || updateResponse.data.length == 0) {
             throw new Error('Failed to upload file to storage.');
         }
+        console.log('Uploaded file:');
+        console.log(updateResponse);
         return updateResponse.data[0].value;
     }
 
